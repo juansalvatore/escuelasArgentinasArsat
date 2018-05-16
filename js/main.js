2583714a1b003d488b38a0d9e95940e51d0e858c
Juan Salvatore <juansalvatore@live.com.ar>
2018-05-16 11:39:54 -0300

    Create project boilerplate

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+
+let grayArea = document.getElementById('close-popup')
+let popup = document.getElementById('popup')
+let cruz = document.getElementById('cruz')
+
+grayArea.addEventListener('click', () => {
+    closePopup()
+})
+cruz.addEventListener('click', () => {
+    closePopup()
+})
+
+closePopup = () => {
+    popup.style.opacity = 0
+    grayArea.style.opacity = 0
+    setTimeout(() => {
+        popup.style.display = 'none'
+        grayArea.style.display = 'none'
+    }, 300);
+}
